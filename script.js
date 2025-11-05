adc54734cec6f0c7bb2da88945b21aab2cc25205
Alessio Ceccarini <alessioceccarini23@icloud.com>
2025-11-05 17:26:23 +0100

    end

--- script.js
+++ script.js
@@ -20,13 +20,13 @@ const getBooks = function () {
         const newCard = document.getElementById("card-section")
         newCard.innerHTML += `
         <div class=col col-4 col-md-3 col-lg-2>
-        <div class="card m-2 p-5">
+        <div class="card m-2 p-5 bg-dark-subtle">
         <img src="${book.img}" class="card-img-top" alt="">
-        <div class="card-body d-flex flex-column justify-content-center">
+        <div class="card-body d-flex justify-content-center">
         <h5 class="card-title text-center">title:${book.title}</h5>
         <p class="card-text">${book.category}</p><span><p class="card-text">${book.price} $</p>
-        <a href="#" class="add-to-cart btn btn-primary fs-5 my-2"data-title=${book.title} data-price=${book.price}>AGGIUNGI AL CARRELLO</a>
-        <a href="#" class="remove-from-cart btn btn-primary fs-5">RIMUOVI DAL CARRELLO</a>
+        <a href="#" class="add-to-cart btn btn-info fs-5 my-2"data-title=${book.title} data-price=${book.price}>AGGIUNGI AL CARRELLO</a>
+        <a href="#" class="remove-from-cart btn btn-danger fs-5">RIMUOVI DAL CARRELLO</a>
         </div>
         </div>
       </div>`
@@ -35,6 +35,11 @@ const getBooks = function () {
       btns.forEach((btn) => {
         btn.textContent === "AGGIUNGI AL CARRELLO"
         btn.addEventListener("click", addToCart)
+      })
+      const removeBtns = document.querySelectorAll(".remove-from-cart")
+      removeBtns.forEach((btn) => {
+        btn.textContent === "RIMUOVI DAL CARRELLO"
+        btn.addEventListener("click", removeFromCart)
       })
     })
     .catch((error) => {
@@ -46,14 +51,12 @@ const addToCart = (event) => {
   const button = event.target
   const title = button.dataset.title
   const price = button.dataset.price
-  console.log(` Aggiunto al carrello: ${title} (${price}$)`)
   const cartElement = { title, price }
   cart.push(cartElement)
   localStorage.setItem(KEY, JSON.stringify(cart))
 }
 
-const removeFromCart = (event) => {
-  const removeBtn = event.target
-  localStorage.removeItem("KEY")
+const removeFromCart = () => {
+  localStorage.removeItem(KEY)
 }
 getBooks()
